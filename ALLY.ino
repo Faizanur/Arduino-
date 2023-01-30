1996568e94dea77642eb4426691f5f62210ef074
Md Faizan ur Rehman <88930802+Faizanur@users.noreply.github.com>
2023-01-31 02:16:54 +0530

    Add files via upload

---
+++ ALLY.ino
@@ -0,0 +1,91 @@
+// Master code for wall-e to be upload in Nano Only
+
+#include <SoftwareSerial.h>
+SoftwareSerial BT(0, 1); // RX | TX
+int it = 0;
+char flag = it;
+
+#define LM 5 // LEFT MOTOR alternate pins that can be used are pin 9,10,11
+#define RM 6 // RIGHT MOTOR alternate pins that can be used are pin 9,10,11
+#define IR A0
+
+void reading()
+{
+  it = analogRead(IR);
+  Serial.println("IR vslue");
+  Serial.println(it);
+}
+
+void forward()
+{
+  digitalWrite(LM, HIGH);
+  digitalWrite(RM, HIGH);
+}
+void left()
+{
+  digitalWrite(LM, HIGH);
+  digitalWrite(RM, LOW);
+}
+void right()
+{
+  digitalWrite(LM, LOW);
+  digitalWrite(RM, HIGH);
+}
+
+// analogRead function controls pitch of the motor but only supports with PWM pins that are 3,5,6,9,10,11 in Nano
+void leftturn()
+{
+  analogWrite(LM, 1023);
+  analogWrite(RM, 500);
+}
+void rightturn()
+{
+  analogWrite(LM, 1023);
+  analogWrite(RM, 500);
+}
+
+void boot()
+{
+  forward();
+  delay(3000);
+  left();
+  delay(3000);
+  right();
+  delay(3000);
+  leftturn();
+  delay(3000);
+  rightturn();
+  delay(3000);
+}
+
+void decide()
+{
+  if (it < 50)
+  {
+    leftturn();
+  }
+  else
+  {
+    forward();
+  }
+}
+void setup()
+{
+  Serial.begin(38400);
+  BT.begin(38400);
+  Serial.println("Ready");
+  pinMode(LM, OUTPUT);
+  pinMode(RM, OUTPUT);
+  pinMode(IR, INPUT);
+  boot();
+}
+void loop()
+{
+  if (Serial.available())
+    BT.write(flag);
+  Serial.println("BT");
+  Serial.println(flag);
+  decide();
+  Serial.println(" ");
+  delay(750);
+}
